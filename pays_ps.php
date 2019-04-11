78f92aa26655f90c6f890281a1e9cfc078dc5257
Paul <pavel.strejcek@brainweb.cz>
2019-04-12 01:45:40 +0200

    fixed untranslated messages with long code

--- pays_ps.php
+++ pays_ps.php
@@ -38,7 +38,7 @@ class Pays_PS extends PaymentModule {
     public function __construct() {
         $this->name = 'pays_ps';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Pavel Strejček @ BrainWeb.cz';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6');
